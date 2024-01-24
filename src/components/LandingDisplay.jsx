da33f854b34462e058095f19189e6c08729de2f6
Jose Laurito <jigolaurito93@yahoo.com>
2024-01-24 04:19:32 -0600

    first commit

---
+++ src/components/LandingDisplay.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LandingDisplay() {
+  return (
+    <div>
+      <h1>Jose Laurito</h1>
+    </div>
+  );
+}
+
+export default LandingDisplay;
---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingDisplay from '../components/LandingDisplay'
+
+function Home() {
+  return (
+    <div>
+      <LandingDisplay />
+    </div>
+  )
+}
+
+export default Home
