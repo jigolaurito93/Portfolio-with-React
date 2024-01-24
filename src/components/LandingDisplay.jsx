3079da5c50d49a04e268ea6debd46eb3ff71aa39
Jose Laurito <jigolaurito93@yahoo.com>
2024-01-24 04:30:00 -0600

    installation

--- src/components/LandingDisplay.jsx
+++ src/components/LandingDisplay.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import jigo from "../assets/jigo.jpg";
 
 function LandingDisplay() {
   return (
     <div>
-      <h1>Jose Laurito</h1>
+      <div>
+        <img src={jigo} alt="profile photo" />
+        <h3>Software Engineer</h3>
+        <h1>Jose Laurito</h1>
+        <p>Professional Chef turned Software Engineer, and a proud new Dad.</p>
+        <div>
+          <button>Download CV</button>
+          <button>Contact Me</button>
+        </div>
+        <div>
+          <a href="">
+            <img src="" alt="facebook icon" />
+          </a>
+          <a href="">
+            <img src="" alt="instagram icon" />
+          </a>
+          <a href="">
+            <img src="" alt="linkedIn icon" />
+          </a>
+          <a href="">
+            <img src="" alt="github icon" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
