516fd0470e1f0bfb465b848be1d788460ae6510e
Jose Laurito <jigolaurito93@yahoo.com>
2024-01-25 03:09:04 -0600

    done with lg and xl and bg color

--- src/components/LandingDisplay.jsx
+++ src/components/LandingDisplay.jsx
@@ -4,8 +4,8 @@ import { FacebookIcon, InstagramIcon } from "./Icons";
 
 function LandingDisplay() {
   return (
-    <div className="px-12 h-screen">
-      <div className="flex flex-col justify-center items-center h-screen gap-5 md:flex-row">
+    <div className="px-12 h-screen bg-[#191825]">
+      <div className="flex flex-col justify-center items-center h-screen gap-5 md:flex-row md:gap-20">
         <div className="rounded-full">
           <img
             src={jigo}
@@ -13,17 +13,17 @@ function LandingDisplay() {
             className="w-72 object-cover rounded-full"
           />
         </div>
-        <div className="flex flex-col md:gap-5">
-          <h3 className="text-xl">Software Engineer</h3>
-          <h1 className="text-4xl md:text-4xl">JOSE LAURITO</h1>
-          <p className="">
+        <div className="flex flex-col gap-4 md:gap-5 xl:gap-7">
+          <h3 className="text-xl text-white">Software Engineer</h3>
+          <h1 className="text-3xl md:text-4xl text-white font-bold lg:text-6xl xl:text-6xl">JOSE LAURITO</h1>
+          <p className="text-white text-xs lg:text-lg">
             Professional Chef turned Software Engineer, and a proud new Dad.
           </p>
-          <div className="flex gap-8">
-            <button className="border rounded-full px-4 py-1 text-">
+          <div className="flex gap-3 justify-center">
+            <button className="border rounded-full py-1 text-xs w-28 text-white">
               Download CV
             </button>
-            <button className="border rounded-full px-4 py-1">
+            <button className="border rounded-full px-3 py-1 text-xs w-28 text-white">
               Contact Me
             </button>
           </div>
