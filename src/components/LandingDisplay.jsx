aad9c987efd1f5b45a23acf0444ede09daae3c45
Jose Laurito <jigolaurito93@yahoo.com>
2024-01-25 02:53:48 -0600

    landing page small and medium

--- src/components/LandingDisplay.jsx
+++ src/components/LandingDisplay.jsx
@@ -1,31 +1,40 @@
 import React from "react";
-import jigo from "../assets/jigo.jpg";
+import jigo from "../assets/square_jigo.jpg";
+import { FacebookIcon, InstagramIcon } from "./Icons";
 
 function LandingDisplay() {
   return (
-    <div>
-      <div>
-        <img src={jigo} alt="profile photo" />
-        <h3>Software Engineer</h3>
-        <h1>Jose Laurito</h1>
-        <p>Professional Chef turned Software Engineer, and a proud new Dad.</p>
-        <div>
-          <button>Download CV</button>
-          <button>Contact Me</button>
+    <div className="px-12 h-screen">
+      <div className="flex flex-col justify-center items-center h-screen gap-5 md:flex-row">
+        <div className="rounded-full">
+          <img
+            src={jigo}
+            alt="profile photo"
+            className="w-72 object-cover rounded-full"
+          />
         </div>
-        <div>
-          <a href="">
-            <img src="" alt="facebook icon" />
-          </a>
-          <a href="">
-            <img src="" alt="instagram icon" />
-          </a>
-          <a href="">
-            <img src="" alt="linkedIn icon" />
-          </a>
-          <a href="">
-            <img src="" alt="github icon" />
-          </a>
+        <div className="flex flex-col md:gap-5">
+          <h3 className="text-xl">Software Engineer</h3>
+          <h1 className="text-4xl md:text-4xl">JOSE LAURITO</h1>
+          <p className="">
+            Professional Chef turned Software Engineer, and a proud new Dad.
+          </p>
+          <div className="flex gap-8">
+            <button className="border rounded-full px-4 py-1 text-">
+              Download CV
+            </button>
+            <button className="border rounded-full px-4 py-1">
+              Contact Me
+            </button>
+          </div>
+          <div className="flex gap-5 justify-center">
+            <a href="#" className="w-8">
+              <FacebookIcon />
+            </a>
+            <a href="#" className="w-8">
+              <InstagramIcon />
+            </a>
+          </div>
         </div>
       </div>
     </div>
