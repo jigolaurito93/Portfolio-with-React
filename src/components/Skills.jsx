caea8f06a9e940be236c109c56408a127be70e56
Jose Laurito <jigolaurito93@yahoo.com>
2024-02-25 02:46:37 -0600

    fixed the skills and expanded the gap

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -20,12 +20,13 @@ import {
 function Skills() {
   return (
     <div className="py-10 flex flex-col justify-center items-center font-poppins font-semibold gap-12">
-      <h1 className="text-5xl text-[#BFA181]">Skills</h1>
-      <div className="flex flex-col gap-3">
+      <h1 className="text-3xl text-[#BFA181] md:text-white lg:text-yellow-400 xl:text-blue-200 2xl:text-white">Skills</h1>
+      <div className="flex flex-col gap-8 justify-center items-center">
+        
         {/* Languages */}
-        <div className="w-[550px] flex flex-col gap-3">
+        <div className="w-[40%] md:w-[60%] lg:w-[80%] xl:w-[110%] flex flex-col gap-3">
           <h1 className="text-[#BFA181] text-xl">Languages</h1>
-          <div className="flex gap-5 border-8 h-40 px-7 rounded-3xl">
+          <div className="flex gap-5 border-[4px] h-40 px-7 rounded-3xl lg:py-7">
             <PythonIcon />
             <JavaScriptIcon />
             <HTMLIcon />
@@ -35,9 +36,9 @@ function Skills() {
         {/* Languages */}
 
         {/* Framework */}
-        <div className="w-[550px] flex flex-col gap-3">
+        <div className="w-[40%] md:w-[60%] lg:w-[80%] xl:w-[110%] flex flex-col gap-3">
           <h1 className="text-[#BFA181] text-xl">Framework</h1>
-          <div className="flex gap-5 border-8 h-40 px-7 rounded-3xl">
+          <div className="flex gap-5 border-[4px] h-40 px-7 rounded-3xl lg:py-7 xl:gap-5">
             <ReactIcon />
             <FlaskIcon />
             <TailwindIcon />
@@ -48,9 +49,9 @@ function Skills() {
         {/* Framework */}
 
         {/* Tools */}
-        <div className="w-[550px] flex flex-col gap-3">
+        <div className="w-[40%] md:w-[60%] lg:w-[80%] xl:w-[110%] flex flex-col gap-3">
           <h1 className="text-[#BFA181] text-xl">Tools</h1>
-          <div className="flex gap-5 border-8 h-40 px-7 rounded-3xl items-center">
+          <div className="flex gap-5 border-[4px] h-40 px-7 rounded-3xl items-center lg:py-7 xl:gap-10">
             <NodeJSIcon />
             <div className="w-[100%] bg-white rounded-full">
               <GithubIcon />
