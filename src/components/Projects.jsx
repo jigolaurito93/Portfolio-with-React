051907371ad173df39c92634dff3dcd023ec2235
Jose Laurito <jigolaurito93@yahoo.com>
2024-02-25 04:08:56 -0600

    card flip working

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,30 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import proj1 from "../assets/proj1.jpg";
 import proj2 from "../assets/proj2.jpg";
 import proj3 from "../assets/proj3.jpg";
+
+const cards = [
+  {
+    cardImage: proj1,
+    cardTitle: "Matrimoni",
+  },
+  {
+    cardImage: proj2,
+    cardTitle: "Break Buddy",
+  },
+  {
+    cardImage: proj3,
+    cardTitle: "Lets Go!",
+  },
+];
 
 function Projects() {
   return (
     <div className="font-poppins flex-col flex py-10 justify-center items-center gap-10">
       <h1 className="text-4xl text-[#BFA181]">Projects</h1>
-      <div className="flex gap-10 justify-center">
-        <img src={proj1} alt="" className="w-[10%] h-[15%] rounded-lg" />
-        <img src={proj2} alt="" className="w-[10%] h-[15%] rounded-lg" />
-
-
-        <div>
-          <div
-            className="w-[10%] flex justify-center items-center relative hover:[transform:rotateY(180deg)] duration-1000 cursor-pointer"
-            style={{ backfaceVisibility: "hidden" }}
-          >
-            <img src={proj3} alt="" className="rounded-lg absolute" />
-            <h1 className="text-white text-xl absolute bg-black w-full h-full px-[25%] py-[60%] [transform:rotateY(180deg)]">
-              Matrimoni
-            </h1>
+      <div className="flex gap-24 justify-center">
+        {cards.map((card) => (
+          <div>
+            {/* container */}
+            <div id="container" className="w-[350px] h-[500px] rounded-lg ">
+              {/* card */}
+              <div
+                id="card"
+                className={`w-full h-full relative cursor-pointer duration-1000 [transform-style:preserve-3d]`}
+              >
+                <img
+                  src={card.cardImage}
+                  alt=""
+                  className="rounded-lg w-full h-full shadow-2xl absolute"
+                  id="front"
+                  style={{ backfaceVisibility: "hidden" }}
+                />
+                <h1
+                  id="back"
+                  className="text-white text-xl [transform:rotateY(180deg)] w-full h-full rounded-lg shadow-2xl absolute bg-black flex justify-center items-center"
+                  style={{ backfaceVisibility: "hidden" }}
+                >
+                  {card.cardTitle}
+                </h1>
+              </div>
+            </div>
+            {/* container */}
           </div>
-        </div>
-
-        
+        ))}
       </div>
     </div>
   );
