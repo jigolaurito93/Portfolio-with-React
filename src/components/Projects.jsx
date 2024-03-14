8451e10c34d3474bc7e70b821989c58ae974bbee
Jose Laurito <jigolaurito93@yahoo.com>
2024-03-14 05:24:55 -0500

    added project images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,37 +2,71 @@ import React, { useState } from "react";
 import proj1 from "../assets/proj1.jpg";
 import proj2 from "../assets/proj2.jpg";
 import proj3 from "../assets/proj3.jpg";
+import proj4 from "../assets/proj4.jpg";
+import proj5 from "../assets/proj5.jpg";
+import proj6 from "../assets/proj6.jpg";
 import { GithubIcon, WebsiteIcon } from "./Icons";
 
 const cards = [
   {
     cardImage: proj1,
     cardTitle: "Matrimoni",
+    description: "HTML, React, JavaScript, Firebase, TailwindCSS",
     github: "https://github.com/Colab-Team-4/matrimoni-react",
     website: "https://matrimoni.vercel.app/",
   },
   {
     cardImage: proj2,
     cardTitle: "Break Buddy",
+    description: "HTML, React, JavaScript, Firebase, TailwindCSS",
     github: "https://github.com/Break-Buddy/Break-Buddy-React",
     website: "https://break-buddy-react.vercel.app/",
   },
   {
     cardImage: proj3,
-    cardTitle: "Lets Go!",
+    cardTitle: "Simon Says Game",
+    description: "HTML, CSS, JavaScript",
+    github: "https://github.com/jigolaurito93/Simon-Says-Game-jQuery",
+    website: "https://simonsays814.netlify.app/",
+  },
+  {
+    cardImage: proj4,
+    cardTitle: "Backpack Website",
+    description: "HTML, CSS, JavaScript",
+    github: "https://github.com/jigolaurito93/Simon-Says-Game-jQuery",
+    website: "https://backpacksite.netlify.app/",
+  },
+  {
+    cardImage: proj5,
+    cardTitle: "Coffee Machine",
+    description: "Python, OOP",
+    github: "https://github.com/jigolaurito93/Coffee-Machine-Python",
+  },
+  {
+    cardImage: proj6,
+    cardTitle: "Tea Store",
+    description: "HTML, CSS, JavaScript",
+    github: "https://github.com/jigolaurito93/Tea-Website-Project",
+    website: "https://teawebsitebaconator.netlify.app/",
   },
 ];
 
 function Projects() {
   return (
-    <div className="font-poppins flex-col flex py-10 justify-center items-center gap-10 scroll-mt-28" id="projects">
+    <div
+      className="font-poppins flex-col flex py-10 justify-center items-center gap-10 scroll-mt-28"
+      id="projects"
+    >
       <h1 className="text-4xl text-[#BFA181]">Projects</h1>
 
-      <div className="flex gap-24 justify-center flex-col xl:flex-row">
-        {cards.map((card) => (
-          <div>
+      <div className="flex gap-24 justify-center lg:px-20 lg:justify-normal flex-col lg:flex-row flex-wrap xl:w-[75%]">
+        {cards.map((card, i) => (
+          <div key={i}>
             {/* container */}
-            <div id="container" className="w-[350px] h-[500px] rounded-lg ">
+            <div
+              id="container"
+              className="w-[350px] h-[500px] lg:w-[200px] lg:h-[300px] rounded-lg "
+            >
               {/* card */}
               <div
                 id="card"
@@ -47,20 +81,32 @@ function Projects() {
                 />
                 <h1
                   id="back"
-                  className="text-white text-3xl [transform:rotateY(180deg)] w-full h-full rounded-lg shadow-2xl absolute bg-black flex justify-center items-center"
+                  className="text-white text-3xl px-2 text-center mx-auto [transform:rotateY(180deg)] w-full h-full rounded-lg shadow-2xl absolute gap-4 bg-black flex flex-col justify-center items-center"
                   style={{ backfaceVisibility: "hidden" }}
                 >
                   {card.cardTitle}
+
+                  <span className="text-lg lg:text-sm text-center px-4">
+                    {card.description}
+                  </span>
                 </h1>
               </div>
             </div>
             <div className="mt-4 flex gap-4 items-center">
-              <a href={card.github} className="w-6 bg-white rounded-full cursor-pointer">
+              <a
+                href={card.github}
+                className="w-6 bg-white rounded-full cursor-pointer"
+              >
                 <GithubIcon />
               </a>
-              <a href={card.website} className="w-6 bg-white rounded-full cursor-pointer">
-                <WebsiteIcon />
-              </a>
+              {i !== 4 && (
+                <a
+                  href={card.website}
+                  className="w-6 bg-white rounded-full cursor-pointer"
+                >
+                  <WebsiteIcon />
+                </a>
+              )}
             </div>
             {/* container */}
           </div>
